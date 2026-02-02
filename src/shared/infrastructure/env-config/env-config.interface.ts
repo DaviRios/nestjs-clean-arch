4bab0f261d14d730c276aa1ea7a6347b3de8a018
Davi Rios dos Santos <AstroSurfer.50@Gmail.com>
2026-02-02 13:31:07 -0300

    Implementing EnvConfig interface

--- src/shared/infrastructure/env-config/env-config.interface.ts
+++ src/shared/infrastructure/env-config/env-config.interface.ts
@@ -1,4 +1,4 @@
 export interface EnvConfig {
-  getAppPort(): number //porta da aplicacao
-  getNodeEnv(): string //env da aplicacao
+  getAppPort(): number //app port
+  getNodeEnv(): string //app env
 }
--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common'
+import { EnvConfig } from './env-config.interface'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  constructor(private configService: ConfigService) {} // get method
+  getAppPort(): number {
+    return Number(this.configService.get<number>('PORT')) //check if this if number and if get is number
+  }
+  getNodeEnv(): string {
+    return this.configService.get<string>('NodeEnv') as string //always double check(get string as string)
+  }
+}
