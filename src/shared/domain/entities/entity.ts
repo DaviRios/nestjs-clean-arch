7e889ae82820308c0bad5fa6412bbcd819285dc4
Davi Rios dos Santos <AstroSurfer.50@Gmail.com>
2026-02-10 19:53:36 -0300

    added StubEntity

---
+++ src/shared/domain/entities/entity.ts
@@ -0,0 +1,22 @@
+import { randomUUID as uuidv4 } from 'node:crypto'
+
+export abstract class Entity<Props = any> {
+  public readonly _id: string
+  public readonly props: Props
+
+  constructor(props: Props, id?: string) {
+    this._id = id || uuidv4()
+    this.props = props
+  }
+
+  get id() {
+    return this._id
+  }
+  //here we converts entity to object
+  toJSON(): Required<{ id: string } & Props> {
+    return {
+      id: this._id,
+      ...this.props,
+    } as Required<{ id: string } & Props>
+  }
+}
