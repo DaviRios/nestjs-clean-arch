67cc866c31220e723b27fa6c6d998c37a224ce81
Davi Rios dos Santos <AstroSurfer.50@Gmail.com>
2026-02-10 18:44:46 -0300

    Adding User data builder method

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,42 @@
+import { UserEntity, UserProps } from '../../user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+
+describe('UserEntity unit tests', () => {
+  let props: UserProps
+  let sut: UserEntity
+
+  beforeEach(() => {
+    props = UserDataBuilder({})
+    sut = new UserEntity(props)
+  })
+
+  it('Constructor method', () => {
+    expect(sut.props.name).toEqual(props.name)
+    expect(sut.props.email).toEqual(props.email)
+    expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+
+  it('Getter name method', () => {
+    expect(sut.props.name).toBeDefined()
+    expect(sut.name).toEqual(props.name)
+    expect(typeof sut.name).toBe('string')
+  })
+
+  it('Getter email method', () => {
+    expect(sut.props.email).toBeDefined()
+    expect(sut.email).toEqual(props.email)
+    expect(typeof sut.email).toBe('string')
+  })
+
+  it('Getter password method', () => {
+    expect(sut.props.password).toBeDefined()
+    expect(sut.password).toEqual(props.password)
+    expect(typeof sut.password).toBe('string')
+  })
+
+  it('Getter createdAt method', () => {
+    expect(sut.props.createdAt).toBeDefined()
+    expect(sut.createdAt).toBeInstanceOf(Date)
+  })
+})
