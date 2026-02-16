ab57884c3431092552ecbe23f07fb7ec80dca3fa
Davi Rios dos Santos <AstroSurfer.50@Gmail.com>
2026-02-16 14:41:50 -0300

    adding update methods and tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -23,16 +23,31 @@ describe('UserEntity unit tests', () => {
     expect(typeof sut.name).toBe('string')
   })
 
+  it('Setter name method', () => {
+    sut.updateName('new name')
+    expect(sut.name).toEqual('new name')
+  })
+
   it('Getter email method', () => {
     expect(sut.props.email).toBeDefined()
     expect(sut.email).toEqual(props.email)
     expect(typeof sut.email).toBe('string')
   })
 
+  it('Setter email method', () => {
+    sut.updateEmail('new@email.com')
+    expect(sut.email).toEqual('new@email.com')
+  })
+
   it('Getter password method', () => {
     expect(sut.props.password).toBeDefined()
     expect(sut.password).toEqual(props.password)
     expect(typeof sut.password).toBe('string')
+  })
+
+  it('Setter email method', () => {
+    sut.updateEmail('123')
+    expect(sut.email).toEqual('123')
   })
 
   it('Getter createdAt method', () => {
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -16,14 +16,38 @@ export class UserEntity extends Entity<UserProps> {
     this.props.createdAt = this.props.createdAt ?? new Date()
   }
 
+  updateName(value: string): void {
+    this.name = value
+  }
+  updateEmail(value: string): void {
+    this.email = value
+  }
+  updatePassword(value: string): void {
+    this.password = value
+  }
+
   get name() {
     return this.props.name
   }
+
+  private set name(value: string) {
+    this.props.name = value
+  }
+
   get email() {
     return this.props.email
   }
+
+  private set email(value: string) {
+    this.props.email = value
+  }
+
   get password() {
     return this.props.password
+  }
+
+  private set password(value: string) {
+    this.props.password = value
   }
   get createdAt() {
     return this.props.createdAt
